f6e1284bb99dbeb5da05b932ce38bc4a52b3279a
Christiaan Swanepoel <christiaan.j.s@gmail.com>
2018-11-08 12:12:42 +1300

    Rough framework for score function test

--- src/beast/validation/ScoreFunctionValidation.java
+++ src/beast/validation/ScoreFunctionValidation.java
@@ -5,28 +5,50 @@ import beast.core.Input;
 import beast.core.Runnable;
 import beast.core.parameter.RealParameter;
 import beast.evolution.speciation.SpeciesTreeDistribution;
+import beast.evolution.tree.Tree;
 
 import java.util.List;
 
-public class ScoreFunctionValidation extends Runnable {
+public class ScoreFunctionValidation extends StochasticValidationTest {
+
+    public ScoreFunctionValidation(){}
+
+    public ScoreFunctionValidation(SpeciesTreeDistribution likelihood, TreeSimulator simulator, List<RealParameter> parameters){
+        setInputValue(likelihoodInput.getName(), likelihood);
+        setInputValue(simulatorInput.getName(), simulator);
+        setInputValue(parametersInput.getName(), parameters);
+    }
 
     private static final int DEFAULT_N_SAMPLES = 1000000;
     private static final double DEFAULT_H = 1e-16;
 
-    public Input<List<RealParameter>> parametersInput = new Input<>("parameters", "Parameters to be included in validation", Input.Validate.REQUIRED);
     public Input<SpeciesTreeDistribution> likelihoodInput = new Input<>("likelihood", "Likelihood to be tested", Input.Validate.REQUIRED);
     public Input<TreeSimulator> simulatorInput = new Input<>("treeSimulation", "Simulator to be tested", Input.Validate.REQUIRED);
+    public Input<List<RealParameter>> parametersInput = new Input<>("parameters", "Parameters to be included in validation", Input.Validate.REQUIRED);
 
     public Input<Integer> nSamplesInput = new Input<>("nSamples", "Number of samples to use", DEFAULT_N_SAMPLES, Input.Validate.OPTIONAL);
     public Input<Double> hInput = new Input<>("h", "Step size to use when calculating gradient", DEFAULT_H, Input.Validate.OPTIONAL);
 
+    private SpeciesTreeDistribution likelihood;
+    private TreeSimulator simulator;
+    private List<RealParameter> parameters;
+
     @Override
-    public void run() throws Exception {
-        
+    public double performTest() {
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public String getDescription() {
+        return String.format("Score function validation test: %s and %s", simulator.getClass().getName(), likelihood.getClass().getName());
     }
 
     @Override
     public void initAndValidate() {
+        super.initAndValidate();
 
+        likelihood = likelihoodInput.get();
+        simulator = simulatorInput.get();
+        parameters = parametersInput.get();
     }
 }
---
+++ src/beast/validation/StochasticValidationTest.java
@@ -0,0 +1,36 @@
+package beast.validation;
+
+import beast.core.Input;
+import beast.core.Runnable;
+import beast.core.parameter.RealParameter;
+
+public abstract class StochasticValidationTest extends Runnable {
+
+    private static final double DEFAULT_ALPHA = 1e-3;
+
+    public Input<Double> alphaInput = new Input<>("alpha", "1 - significance level of test", DEFAULT_ALPHA);
+
+    private double alpha;
+
+    public abstract double performTest();
+
+    public abstract String getDescription();
+
+    public void initAndValidate(){
+        alpha = alphaInput.get();
+
+        if(alpha <= 0.0 || alpha >= 1.0) throw new IllegalArgumentException("alpha must be between 0 and 1");
+    }
+
+    @Override
+    public void run(){
+        System.out.println("Stochastic validation test");
+        System.out.println(getDescription());
+        System.out.println("Performing test...");
+        double pValue = performTest();
+        System.out.println("Test complete");
+        System.out.println(String.format("p value: %f", pValue));
+        System.out.println(String.format("%s at significance level %f", pValue < alpha ? "FAILED" : "PASSED", 1 - alpha));
+    }
+
+}
