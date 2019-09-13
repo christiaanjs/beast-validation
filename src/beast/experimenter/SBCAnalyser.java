68ac57289781af7b6f1c6fbe7400d21fe9bfd808
Remco Bouckaert <higgs.ml@gmail.com>
2019-09-13 15:20:13 +1200

    add SVG output to SBCAnalyser

--- src/beast/experimenter/SBCAnalyser.java
+++ src/beast/experimenter/SBCAnalyser.java
@@ -1,14 +1,14 @@
 package beast.experimenter;
 
 import java.io.File;
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
+import java.io.PrintStream;
 import java.util.Arrays;
 
 import org.apache.commons.math.distribution.BinomialDistribution;
 import org.apache.commons.math.distribution.BinomialDistributionImpl;
 
 import beast.app.util.Application;
+import beast.app.util.OutFile;
 import beast.core.Citation;
 import beast.core.Description;
 import beast.core.Input;
@@ -23,42 +23,66 @@ public class SBCAnalyser extends Runnable {
 	final public Input<File> logFileInput = new Input<>("log", "log file containing actual values", Validate.REQUIRED);
 	final public Input<Integer> skipLogLinesInput = new Input<>("skip", "numer of log file lines to skip", 1);
 	final public Input<File> logAnalyserFileInput = new Input<>("logAnalyser", "file produced by loganalyser tool using the -oneline option, containing estimated values", Validate.REQUIRED);
-	final public Input<Integer> binCountInput = new Input<>("bins", "number of bins to represent prior distribution", 20);
+	final public Input<Integer> binCountInput = new Input<>("bins", "number of bins to represent prior distribution", 21);
 
+	final public Input<OutFile> outputInput = new Input<>("outputDir", "output directory for SVG bar charts",
+			new OutFile("[[none]]"));
+	
+	final public Input<Boolean> useRankedBinsInput = new Input<>("useRankedBins", "if true use ranking wrt prior to find bins."
+			+ "if false, use empirical bins based on prior.", true);
+
+	
 	@Override
 	public void initAndValidate() {
 	}
 
 	@Override
 	public void run() throws Exception {
+		File svgdir = null;
+		PrintStream html = null;
+		if (outputInput.get() != null && !outputInput.get().getName().equals("[[none]]")) {
+			svgdir = outputInput.get();
+			if (!svgdir.isDirectory()) {
+				throw new IllegalArgumentException(svgdir.getPath() + " is not a directory. Specify an existing directory for outputDir");
+			}
+			html = new PrintStream(svgdir.getPath()+"/SBC.html");
+			html.println("<!doctype html>\n"+
+					"<html>\n"+
+					"<head><title>Simulation Based Calibration</title></head>\n"+
+					"<body>\n");
+			html.println("<h2>Simulation Based Calibration</h2>");
+			html.println("<li>prior sample: " + logFileInput.get().getPath()+"</li>");
+			html.println("<li>posterior samples: " + logAnalyserFileInput.get().getPath()+"</li>");
+			html.println("<li>Use "+ (useRankedBinsInput.get() ? "ranking" : "empirical bins") + " for bins</li>");
+		}
+		
+
 		LogAnalyser truth = new LogAnalyser(logFileInput.get().getAbsolutePath(), 0, true, false);
 		LogAnalyser estimated = new LogAnalyser(logAnalyserFileInput.get().getAbsolutePath(), 0, true, false);
 		int skip = skipLogLinesInput.get();
-//		if (truth.getTrace(0).length - skip != estimated.getTrace(0).length) {
-//			Log.warning("WARNING: traces are of different lengths: "
-//					+ (truth.getTrace(0).length - skip) + "!=" + estimated.getTrace(0).length);
-//		}
-		
-//		NumberFormat formatter = new DecimalFormat("#0.00");
-		
+
 		int binCount = binCountInput.get();
+		
+		BinomialDistribution binom = new BinomialDistributionImpl(truth.getTrace(0).length, 1.0/binCount);
+		int pLow = binom.inverseCumulativeProbability(0.005);
+		int pUp = binom.inverseCumulativeProbability(0.995);
+		int pLow95 = binom.inverseCumulativeProbability(0.025);
+		int pUp95 = binom.inverseCumulativeProbability(0.975);
+		int pExp = truth.getTrace(0).length/binCount;
+		Log.info("99%lo << mean << 99%up = " + pLow + " << " + pExp + " << " + pUp);
+		
 		StringBuilder b = new StringBuilder();
 		for (int j = 0; j < binCount; j++) {
 			b.append("bin" + j + "\t");
 		}
-		Log.info(CoverageCalculator.space + "\tmissed"
-//				+ "\tMean ESS\tMin ESS\t" 
-				+ b.toString());
-		BinomialDistribution binom = new BinomialDistributionImpl(estimated.getTrace(0).length, 1.0/binCount);
-		int pLow = binom.inverseCumulativeProbability(0.025);
-		int pUp = binom.inverseCumulativeProbability(0.975);
+		Log.info(CoverageCalculator.space + "\tmissed\t" + b.toString());
+		
 		for (int i = 0; i < truth.getLabels().size(); i++) {
 			String label = truth.getLabels().get(i);
 			Double [] trueValues = truth.getTrace(label);
 						
-			Double [] means = estimated.getTrace(label);
-//			Double [] ess = estimated.getTrace(label+".ESS");
-			if (means == null) {
+			Double [] estimates = estimated.getTrace(label);
+			if (estimates == null) {
 				Log.warning("Skipping " + label);
 			} else {
 				if (skip > 0) {
@@ -68,34 +92,46 @@ public class SBCAnalyser extends Runnable {
 				}
 
 				int  [] bins = new int[binCount];
-				Arrays.sort(trueValues);
+				//Arrays.sort(trueValues);
+				Arrays.sort(estimates);
+				
 //				double [] binBoundaries = new double[binCount - 1];
 //				for (int k = 0; k < binCount-1; k++) {
 //					int j = (int) (trueValues.length * (k+1.0)/binCount);
 //					binBoundaries[k] = (trueValues[j] + trueValues[j+1]) / 2.0;
 //				}
 
-//				double minESS = Double.POSITIVE_INFINITY;
-//				double meanESS = 0;
-				for (int j = 0; j < trueValues.length - skip; j++) {
-//					int bin = Arrays.binarySearch(binBoundaries, means[j]);
-//					if (bin < 0) {
-//						bin = 1-bin;
-//					}
-//					bins[bin]++;
-					
-
-					int rank = Arrays.binarySearch(trueValues, means[j]);
-					if (rank < 0) {
-						rank = 1-rank;
+				boolean empiricalBins = useRankedBinsInput.get();
+				double [] binBoundaries = new double[binCount - 1];
+				if (empiricalBins) {
+					for (int k = 0; k < binCount-1; k++) {
+						int j = (int) (estimates.length * (k+1.0)/binCount);
+						binBoundaries[k] = (estimates[j] + estimates[j+1]) / 2.0;
 					}
-					int bin = rank * (binCount-1) / trueValues.length;
-					bins[bin]++;
-					
-//					minESS = Math.min(minESS, ess[j]);
-//					meanESS += ess[j];
 				}
-//				meanESS /= (trueValues.length - skip);
+
+				
+				for (int j = 0; j < trueValues.length; j++) {
+					if (empiricalBins) {
+						int bin = Arrays.binarySearch(binBoundaries, trueValues[j]);
+						if (bin < 0) {
+							bin = -bin-1;
+						}
+						bins[bin]++;
+					} else {
+						int rank = Arrays.binarySearch(estimates, trueValues[j]);
+						if (rank < 0) {
+							rank = 1-rank;
+						}
+						int bin = rank * binCount / estimates.length;
+						if (bin == bins.length) {
+							bin--;
+						}
+						bins[bin]++;
+					}
+					
+				}
+
 				
 				b = new StringBuilder();
 				int missed = 0;
@@ -108,13 +144,68 @@ public class SBCAnalyser extends Runnable {
 				
 				Log.info(label + (label.length() < CoverageCalculator.space.length() ? CoverageCalculator.space.substring(label.length()) : "") + "\t" + 
 						missed + "\t" + 
-//						formatter.format(meanESS) + "\t" + 
-//						formatter.format(minESS) + "\t" +
 						b.toString());
+
+				if (html != null) {
+					int max = pUp;
+					for (int d : bins) {
+						max = Math.max(d,  max);
+					}
+					if (max < 100) {
+						max = max + 9 - (max+9) % 10;
+					} else if (max < 250) {
+						max = max + 49 - (max+49) % 50;
+					} else {
+						max = max + 99 - (max+99) % 100;
+					}
+					
+					PrintStream svg = new PrintStream(svgdir.getPath() +"/" + label + ".svg");
+					svg.println("<svg class=\"chart\" width=\"1044\" height=\"760\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\">");
+					
+					// axes
+					svg.println("<rect x='15' width=\"1000\" height=\"700\" style=\"fill:none;stroke-width:1;stroke:rgb(0,0,0)\"/>");
+					
+					// bars
+					double dx = 1000.0 / binCount;
+					double dy = 740.0 / max;
+					// permissable area
+					svg.println("<g transform=\"translate(20,700) scale("+dx+",-"+dy+")\">");
+					svg.println("  <polygon points=\"0,"+pExp+" "+binCount+","+pExp+" "+(binCount+1)+","+pUp+" -1,"+pUp+"\" style=\"fill:#eee;stroke-width:0;stroke:rgb(0,0,0);opacity: 0.5;\"/>");
+					svg.println("  <polygon points=\"0,"+pExp+" "+binCount+","+pExp+" "+(binCount+1)+","+pLow+" -1,"+pLow+"\" style=\"fill:#eee;stroke-width:0;stroke:rgb(0,0,0);opacity: 0.5;\"/>");
+					svg.println("  <polygon points=\"0,"+pExp+" "+binCount+","+pExp+" "+(binCount+1)+","+pUp95+" -1,"+pUp95+"\" style=\"fill:#ccc;stroke-width:0;stroke:rgb(0,0,0);opacity: 0.5;\"/>");
+					svg.println("  <polygon points=\"0,"+pExp+" "+binCount+","+pExp+" "+(binCount+1)+","+pLow95+" -1,"+pLow95+"\" style=\"fill:#ccc;stroke-width:0;stroke:rgb(0,0,0);opacity: 0.5;\"/>");
+
+					
+					for (int j = 0; j < binCount; j++) {
+						svg.println("  <rect x=\""+j+"\" y=\"0\" width=\"0.9\" height=\""+bins[j]+"\" style=\"fill:#faa;\"/>");
+						if (j % 10 == 0) {
+						// ticks
+						//	svg.println("<line y1='0' y2='-1' x1='" + (j+0.45) + "' x2='" + (j+0.45) + "' style='stroke-width:0.1;stroke:rgb(0,0,0)'/>");
+						}
+					}
+					svg.println("</g>");
+					svg.println("<text x='0' y='10'>" + max + "</text>");
+					svg.println("<text x='0' y='700'>0</text>");
+					for (int j = 0; j < binCount; j+=10) {
+						int x = 10 + j*1000/binCount + 500/binCount;
+						int y = 720;
+						svg.println("<text x='"+x+"' y='"+y+"'>" + j + "</text>");
+					}
+					svg.println("</svg>");
+					svg.close();
+					
+					html.println("<h3>" + label + "</h3>");
+					html.println("<img src=\"" + label + ".svg\">");
+					html.println("<p>Missed: " + missed);
+				}
 			}
 		}
-		Log.info("Expected number of misses: " + 0.05 * binom.getNumberOfTrials());
+		//Log.info("Expected number of misses: " + 0.05 * binom.getNumberOfTrials());
 		
+		if (html != null) {
+			//html.println("Expected number of misses: " + 0.05 * binom.getNumberOfTrials());
+			html.println("</body>\n</html>");
+		}
 
 	
 	}
